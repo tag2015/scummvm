0b1486441ae61c0175ed856d61bdeaf186afe9f9
Walter Agazzi <walter.agazzi@protonmail.com>
2023-05-05 02:38:56 +0200

    FALLOUT2: Added original implementation (fallout2ce)

---
+++ engines/fallout2/memory.h
@@ -0,0 +1,15 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include "memory_defs.h"
+
+namespace fallout {
+
+char* internal_strdup(const char* string);
+void* internal_malloc(size_t size);
+void* internal_realloc(void* ptr, size_t size);
+void internal_free(void* ptr);
+
+} // namespace fallout
+
+#endif /* MEMORY_H */
