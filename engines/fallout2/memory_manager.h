9531649460e9b0f6381362bea4bf1ff383e4532d
Walter Agazzi <walter.agazzi@protonmail.com>
2023-05-07 23:19:30 +0200

    FALLOUT2: Implement memory functions

--- engines/fallout2/memory_manager.h
+++ engines/fallout2/memory_manager.h
@@ -1,17 +1,17 @@
-#ifndef MEMORY_MANAGER_H
-#define MEMORY_MANAGER_H
+#ifndef FALLOUT2_MEMORY_MANAGER_H
+#define FALLOUT2_MEMORY_MANAGER_H
 
-#include "memory_defs.h"
+#include "fallout2/memory_defs.h"
 
-namespace fallout {
+namespace Fallout2 {
 
-void memoryManagerSetProcs(MallocProc* mallocProc, ReallocProc* reallocProc, FreeProc* freeProc);
-void* internal_malloc_safe(size_t size, const char* file, int line);
-void* internal_realloc_safe(void* ptr, size_t size, const char* file, int line);
-void internal_free_safe(void* ptr, const char* file, int line);
-void* internal_calloc_safe(int count, int size, const char* file, int line);
-char* strdup_safe(const char* string, const char* file, int line);
+void memoryManagerSetProcs(MallocProc *mallocProc, ReallocProc *reallocProc, FreeProc *freeProc);
+void *internal_malloc_safe(size_t size, const char *file, int line);
+void *internal_realloc_safe(void *ptr, size_t size, const char *file, int line);
+void internal_free_safe(void *ptr, const char *file, int line);
+void *internal_calloc_safe(int count, int size, const char *file, int line);
+char *strdup_safe(const char *string, const char *file, int line);
 
-} // namespace fallout
+} // namespace Fallout2
 
-#endif /* MEMORY_MANAGER_H */
+#endif
