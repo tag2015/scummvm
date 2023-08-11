d02f8521ba0a6e080dd23d1685ff2bc5328b228b
Walter Agazzi <walter.agazzi@protonmail.com>
2023-08-12 00:01:14 +0200

    FALLOUT2: Update module.mk

--- engines/fallout2/module.mk
+++ engines/fallout2/module.mk
@@ -5,18 +5,30 @@ MODULE_OBJS = \
 	console.o \
 	metaengine.o \
 	cache.o \
+	color.o \
+	config.o \
+	cycle.o \
+	db.o \
 	dfile.o \
 	dictionary.o \
 	draw.o \
 	file_find.o \
+	font_manager.o \
+	game_config.o \
+	game_memory.o \
 	geometry.o \
 	graph_lib.o \
 	heap.o \
 	memory.o \
 	memory_manager.o \
+	palette.o \
+	pcx.o \
 	platform_compat.o \
 	random.o \
+	settings.o \
+	text_font.o \
 	version.o \
+	xfile.o \
 	third_party/fpattern/fpattern.o
 
 # This module can be built as a plugin
