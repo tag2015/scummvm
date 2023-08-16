af5cf359533aed75baa80104e2d61e4f17f24548
Walter Agazzi <walter.agazzi@protonmail.com>
2023-08-16 12:21:04 +0200

    FALLOUT2: Implement debug.h

--- engines/fallout2/debug.h
+++ engines/fallout2/debug.h
@@ -1,19 +1,27 @@
-#ifndef DEBUG_H
-#define DEBUG_H
+#ifndef FALLOUT2_DEBUG_H
+#define FALLOUT2_DEBUG_H
 
-namespace fallout {
+#include "common/debug.h"
 
-typedef int(DebugPrintProc)(char* string);
+namespace Fallout2 {
+
+/* typedef int(DebugPrintProc)(char *string);
 
 void _GNW_debug_init();
 void _debug_register_mono();
-void _debug_register_log(const char* fileName, const char* mode);
+void _debug_register_log(const char *fileName, const char *mode);
 void _debug_register_screen();
 void _debug_register_env();
-void _debug_register_func(DebugPrintProc* proc);
-int debugPrint(const char* format, ...);
+void _debug_register_func(DebugPrintProc *proc);
+int debugPrint(const char *format, ...);
 void _debug_exit(void);
+*/
 
-} // namespace fallout
+template<typename... Args>
+void debugPrint(const char *format, Args... args) {
+	debug(format, args...);
+}
+
+} // namespace Fallout2
 
 #endif /* DEBUG_H */
