ad07d7efc82d91af6ff08f941bee824a7cbe0417
Walter Agazzi <walter.agazzi@protonmail.com>
2023-08-17 13:19:11 +0200

    FALLOUT2: Implement dinput.h/cpp

--- engines/fallout2/dinput.h
+++ engines/fallout2/dinput.h
@@ -1,9 +1,11 @@
-#ifndef DINPUT_H
-#define DINPUT_H
+#ifndef FALLOUT2_DINPUT_H
+#define FALLOUT2_DINPUT_H
 
-#include <SDL.h>
+// #include <SDL.h>
 
-namespace fallout {
+#include "common/events.h"
+
+namespace Fallout2 {
 
 typedef struct MouseData {
 	int x;
@@ -22,19 +24,19 @@ bool directInputInit();
 void directInputFree();
 bool mouseDeviceAcquire();
 bool mouseDeviceUnacquire();
-bool mouseDeviceGetData(MouseData* mouseData);
+bool mouseDeviceGetData(MouseData *mouseData);
 bool keyboardDeviceAcquire();
 bool keyboardDeviceUnacquire();
 bool keyboardDeviceReset();
-bool keyboardDeviceGetData(KeyboardData* keyboardData);
+bool keyboardDeviceGetData(KeyboardData *keyboardData);
 bool mouseDeviceInit();
 void mouseDeviceFree();
 bool keyboardDeviceInit();
 void keyboardDeviceFree();
 
-void handleMouseEvent(SDL_Event* event);
-void handleTouchEvent(SDL_Event* event);
+void handleMouseEvent(const Common::Event *event);
+void handleTouchEvent(const Common::Event *event);
 
-} // namespace fallout
+} // namespace Fallout2
 
-#endif /* DINPUT_H */
+#endif
