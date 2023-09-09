8b0e06f8001d42708da862448c58eec9e952aaab
Walter Agazzi <walter.agazzi@protonmail.com>
2023-09-09 20:45:18 +0200

    FALLOUT2: Implement various headers

--- engines/fallout2/trait.h
+++ engines/fallout2/trait.h
@@ -1,25 +1,25 @@
-#ifndef TRAIT_H
-#define TRAIT_H
+#ifndef FALLOUT2_TRAIT_H
+#define FALLOUT2_TRAIT_H
 
-#include "db.h"
-#include "trait_defs.h"
+#include "fallout2/db.h"
+#include "fallout2/trait_defs.h"
 
-namespace fallout {
+namespace Fallout2 {
 
 int traitsInit();
 void traitsReset();
 void traitsExit();
-int traitsLoad(File* stream);
-int traitsSave(File* stream);
+int traitsLoad(File *stream);
+int traitsSave(File *stream);
 void traitsSetSelected(int trait1, int trait2);
-void traitsGetSelected(int* trait1, int* trait2);
-char* traitGetName(int trait);
-char* traitGetDescription(int trait);
+void traitsGetSelected(int *trait1, int *trait2);
+char *traitGetName(int trait);
+char *traitGetDescription(int trait);
 int traitGetFrmId(int trait);
 bool traitIsSelected(int trait);
 int traitGetStatModifier(int stat);
 int traitGetSkillModifier(int skill);
 
-} // namespace fallout
+} // namespace Fallout2
 
-#endif /* TRAIT_H */
+#endif
