2c4d2dd4dd5650b405308da7488d31cc22cfbace
Walter Agazzi <walter.agazzi@protonmail.com>
2023-09-09 20:45:18 +0200

    FALLOUT2: Implement various headers

--- engines/fallout2/stat.h
+++ engines/fallout2/stat.h
@@ -1,44 +1,44 @@
-#ifndef STAT_H
-#define STAT_H
+#ifndef FALLOUT2_STAT_H
+#define FALLOUT2_STAT_H
 
-#include "db.h"
-#include "obj_types.h"
-#include "proto_types.h"
-#include "stat_defs.h"
+#include "fallout2/db.h"
+#include "fallout2/obj_types.h"
+#include "fallout2/proto_types.h"
+#include "fallout2/stat_defs.h"
 
-namespace fallout {
+namespace Fallout2 {
 
 #define STAT_ERR_INVALID_STAT (-5)
 
 int statsInit();
 int statsReset();
 int statsExit();
-int statsLoad(File* stream);
-int statsSave(File* stream);
-int critterGetStat(Object* critter, int stat);
-int critterGetBaseStatWithTraitModifier(Object* critter, int stat);
-int critterGetBaseStat(Object* critter, int stat);
-int critterGetBonusStat(Object* critter, int stat);
-int critterSetBaseStat(Object* critter, int stat, int value);
-int critterIncBaseStat(Object* critter, int stat);
-int critterDecBaseStat(Object* critter, int stat);
-int critterSetBonusStat(Object* critter, int stat, int value);
-void protoCritterDataResetStats(CritterProtoData* data);
-void critterUpdateDerivedStats(Object* critter);
-char* statGetName(int stat);
-char* statGetDescription(int stat);
-char* statGetValueDescription(int value);
+int statsLoad(File *stream);
+int statsSave(File *stream);
+int critterGetStat(Object *critter, int stat);
+int critterGetBaseStatWithTraitModifier(Object *critter, int stat);
+int critterGetBaseStat(Object *critter, int stat);
+int critterGetBonusStat(Object *critter, int stat);
+int critterSetBaseStat(Object *critter, int stat, int value);
+int critterIncBaseStat(Object *critter, int stat);
+int critterDecBaseStat(Object *critter, int stat);
+int critterSetBonusStat(Object *critter, int stat, int value);
+void protoCritterDataResetStats(CritterProtoData *data);
+void critterUpdateDerivedStats(Object *critter);
+char *statGetName(int stat);
+char *statGetDescription(int stat);
+char *statGetValueDescription(int value);
 int pcGetStat(int pcStat);
 int pcSetStat(int pcStat, int value);
 void pcStatsReset();
 int pcGetExperienceForNextLevel();
 int pcGetExperienceForLevel(int level);
-char* pcStatGetName(int pcStat);
-char* pcStatGetDescription(int pcStat);
+char *pcStatGetName(int pcStat);
+char *pcStatGetDescription(int pcStat);
 int statGetFrmId(int stat);
-int statRoll(Object* critter, int stat, int modifier, int* howMuch);
-int pcAddExperience(int xp, int* xpGained = NULL);
-int pcAddExperienceWithOptions(int xp, bool a2, int* xpGained = NULL);
+int statRoll(Object *critter, int stat, int modifier, int *howMuch);
+int pcAddExperience(int xp, int *xpGained = NULL);
+int pcAddExperienceWithOptions(int xp, bool a2, int *xpGained = NULL);
 int pcSetExperience(int a1);
 
 static inline bool statIsValid(int stat) {
@@ -49,6 +49,6 @@ static inline bool pcStatIsValid(int pcStat) {
 	return pcStat >= 0 && pcStat < PC_STAT_COUNT;
 }
 
-} // namespace fallout
+} // namespace Fallout2
 
-#endif /* STAT_H */
+#endif
