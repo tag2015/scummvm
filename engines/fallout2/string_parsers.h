9bd156cecc150c1fb0c3eb60abf13de4ca8b7c68
Walter Agazzi <walter.agazzi@protonmail.com>
2023-09-10 16:19:33 +0200

    FALLOUT2: Implement string_parsers

--- engines/fallout2/string_parsers.h
+++ engines/fallout2/string_parsers.h
@@ -1,16 +1,16 @@
-#ifndef STRING_PARSERS_H
-#define STRING_PARSERS_H
+#ifndef FALLOUT2_STRING_PARSERS_H
+#define FALLOUT2_STRING_PARSERS_H
 
-namespace fallout {
+namespace Fallout2 {
 
-typedef int(StringParserCallback)(char* string, int* valuePtr);
+typedef int(StringParserCallback)(char *string, int *valuePtr);
 
-int strParseInt(char** stringPtr, int* valuePtr);
-int strParseStrFromList(char** stringPtr, int* valuePtr, const char** list, int count);
-int strParseStrFromFunc(char** stringPtr, int* valuePtr, StringParserCallback* callback);
-int strParseIntWithKey(char** stringPtr, const char* key, int* valuePtr, const char* delimeter);
-int strParseKeyValue(char** stringPtr, char* key, int* valuePtr, const char* delimeter);
+int strParseInt(char **stringPtr, int *valuePtr);
+int strParseStrFromList(char **stringPtr, int *valuePtr, const char **list, int count);
+int strParseStrFromFunc(char **stringPtr, int *valuePtr, StringParserCallback *callback);
+int strParseIntWithKey(char **stringPtr, const char *key, int *valuePtr, const char *delimeter);
+int strParseKeyValue(char **stringPtr, char *key, int *valuePtr, const char *delimeter);
 
-} // namespace fallout
+} // namespace Fallout2
 
-#endif /* STRING_PARSERS_H */
+#endif
