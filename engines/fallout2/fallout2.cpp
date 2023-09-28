1609df501b2321aa8cc03bfaf339cc7c14e28de4
Walter Agazzi <walter.agazzi@protonmail.com>
2023-09-28 03:29:49 +0200

    FALLOUT2: Enable kb checks, alternate credits

--- engines/fallout2/fallout2.cpp
+++ engines/fallout2/fallout2.cpp
@@ -627,6 +627,14 @@ Common::Error Fallout2Engine::run() {
 			creditsOpen("credits.txt", -1, false);
 			break;
 
+		case MAIN_MENU_QUOTES:
+			// NOTE: There is a strange cmp at 0x480C50. Both operands are
+			// zero, set before the loop and do not modify afterwards. For
+			// clarity this condition is omitted.
+			mainMenuWindowHide(true);
+			creditsOpen("quotes.txt", -1, true);
+			break;
+
 		case MAIN_MENU_EXIT:
 		case -1:
 			done = true;
