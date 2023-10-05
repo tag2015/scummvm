073eeeb4bc5786e41e2ed0015d5a3ad8f6504c31
Walter Agazzi <walter.agazzi@protonmail.com>
2023-10-05 12:41:34 +0200

    FALLOUT2: Implement automap flags / prefs loading

--- engines/fallout2/preferences.h
+++ engines/fallout2/preferences.h
@@ -10,7 +10,7 @@ namespace Fallout2 {
 int preferencesInit();
 int doPreferences(bool animated);
 int preferencesSave(Common::OutSaveFile *stream);
-int preferencesLoad(File *stream);
+int preferencesLoad(Common::InSaveFile *stream);
 void brightnessIncrease();
 void brightnessDecrease();
 
