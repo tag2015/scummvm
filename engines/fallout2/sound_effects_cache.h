b16a3de4049203d2f4735749c54b08429e24fb92
Walter Agazzi <walter.agazzi@protonmail.com>
2023-10-12 14:50:04 +0200

    FALLOUT2: Implement sound_decoder/sound_effects

--- engines/fallout2/sound_effects_cache.h
+++ engines/fallout2/sound_effects_cache.h
@@ -1,24 +1,24 @@
-#ifndef SOUND_EFFECTS_CACHE_H
-#define SOUND_EFFECTS_CACHE_H
+#ifndef FALLOUT2_SOUND_EFFECTS_CACHE_H
+#define FALLOUT2_SOUND_EFFECTS_CACHE_H
 
-namespace fallout {
+namespace Fallout2 {
 
 // The maximum number of sound effects that can be loaded and played
 // simultaneously.
 #define SOUND_EFFECTS_MAX_COUNT (4)
 
-int soundEffectsCacheInit(int cache_size, const char* effectsPath);
+int soundEffectsCacheInit(int cache_size, const char *effectsPath);
 void soundEffectsCacheExit();
 int soundEffectsCacheInitialized();
 void soundEffectsCacheFlush();
-int soundEffectsCacheFileOpen(const char* fname, int* sampleRate);
+int soundEffectsCacheFileOpen(const char *fname, int *sampleRate);
 int soundEffectsCacheFileClose(int handle);
-int soundEffectsCacheFileRead(int handle, void* buf, unsigned int size);
-int soundEffectsCacheFileWrite(int handle, const void* buf, unsigned int size);
+int soundEffectsCacheFileRead(int handle, void *buf, unsigned int size);
+int soundEffectsCacheFileWrite(int handle, const void *buf, unsigned int size);
 long soundEffectsCacheFileSeek(int handle, long offset, int origin);
 long soundEffectsCacheFileTell(int handle);
 long soundEffectsCacheFileLength(int handle);
 
-} // namespace fallout
+} // namespace Fallout2
 
-#endif /* SOUND_EFFECTS_CACHE_H */
+#endif
