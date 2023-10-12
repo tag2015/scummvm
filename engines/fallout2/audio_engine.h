97cbe68b2a37fe0f6dce78b4176e45f3b3809b90
Walter Agazzi <walter.agazzi@protonmail.com>
2023-10-12 21:34:34 +0200

    FALLOUT2: Preliminary implementation of audio_engine

--- engines/fallout2/audio_engine.h
+++ engines/fallout2/audio_engine.h
@@ -1,7 +1,7 @@
-#ifndef AUDIO_ENGINE_H
-#define AUDIO_ENGINE_H
+#ifndef FALLOUT2_AUDIO_ENGINE_H
+#define FALLOUT2_AUDIO_ENGINE_H
 
-namespace fallout {
+namespace Fallout2 {
 
 #define AUDIO_ENGINE_SOUND_BUFFER_LOCK_FROM_WRITE_POS 0x00000001
 #define AUDIO_ENGINE_SOUND_BUFFER_LOCK_ENTIRE_BUFFER 0x00000002
@@ -18,16 +18,16 @@ void audioEngineResume();
 int audioEngineCreateSoundBuffer(unsigned int size, int bitsPerSample, int channels, int rate);
 bool audioEngineSoundBufferRelease(int soundBufferIndex);
 bool audioEngineSoundBufferSetVolume(int soundBufferIndex, int volume);
-bool audioEngineSoundBufferGetVolume(int soundBufferIndex, int* volumePtr);
+bool audioEngineSoundBufferGetVolume(int soundBufferIndex, int *volumePtr);
 bool audioEngineSoundBufferSetPan(int soundBufferIndex, int pan);
 bool audioEngineSoundBufferPlay(int soundBufferIndex, unsigned int flags);
 bool audioEngineSoundBufferStop(int soundBufferIndex);
-bool audioEngineSoundBufferGetCurrentPosition(int soundBufferIndex, unsigned int* readPosPtr, unsigned int* writePosPtr);
+bool audioEngineSoundBufferGetCurrentPosition(int soundBufferIndex, unsigned int *readPosPtr, unsigned int *writePosPtr);
 bool audioEngineSoundBufferSetCurrentPosition(int soundBufferIndex, unsigned int pos);
-bool audioEngineSoundBufferLock(int soundBufferIndex, unsigned int writePos, unsigned int writeBytes, void** audioPtr1, unsigned int* audioBytes1, void** audioPtr2, unsigned int* audioBytes2, unsigned int flags);
-bool audioEngineSoundBufferUnlock(int soundBufferIndex, void* audioPtr1, unsigned int audioBytes1, void* audioPtr2, unsigned int audioBytes2);
-bool audioEngineSoundBufferGetStatus(int soundBufferIndex, unsigned int* status);
+bool audioEngineSoundBufferLock(int soundBufferIndex, unsigned int writePos, unsigned int writeBytes, void **audioPtr1, unsigned int *audioBytes1, void **audioPtr2, unsigned int *audioBytes2, unsigned int flags);
+bool audioEngineSoundBufferUnlock(int soundBufferIndex, void *audioPtr1, unsigned int audioBytes1, void *audioPtr2, unsigned int audioBytes2);
+bool audioEngineSoundBufferGetStatus(int soundBufferIndex, unsigned int *status);
 
-} // namespace fallout
+} // namespace Fallout2
 
-#endif /* AUDIO_ENGINE_H */
+#endif
