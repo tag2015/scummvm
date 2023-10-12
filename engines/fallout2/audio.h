14cf60ab7021c44afbd6fab94700b9819b7558fc
Walter Agazzi <walter.agazzi@protonmail.com>
2023-10-12 18:22:51 +0200

    FALLOUT2: Implement audio

--- engines/fallout2/audio.h
+++ engines/fallout2/audio.h
@@ -1,20 +1,20 @@
-#ifndef AUDIO_H
-#define AUDIO_H
+#ifndef FALLOUT2_AUDIO_H
+#define FALLOUT2_AUDIO_H
 
-namespace fallout {
+namespace Fallout2 {
 
-typedef bool(AudioQueryCompressedFunc)(char* filePath);
+typedef bool(AudioQueryCompressedFunc)(char *filePath);
 
-int audioOpen(const char* fname, int* sampleRate);
+int audioOpen(const char *fname, int *sampleRate);
 int audioClose(int handle);
-int audioRead(int handle, void* buffer, unsigned int size);
+int audioRead(int handle, void *buffer, unsigned int size);
 long audioSeek(int handle, long offset, int origin);
 long audioGetSize(int handle);
 long audioTell(int handle);
-int audioWrite(int handle, const void* buf, unsigned int size);
-int audioInit(AudioQueryCompressedFunc* func);
+int audioWrite(int handle, const void *buf, unsigned int size);
+int audioInit(AudioQueryCompressedFunc *func);
 void audioExit();
 
-} // namespace fallout
+} // namespace Fallout2
 
-#endif /* AUDIO_H */
+#endif
