ded8c040e0f03de323e146563a1eeaf195504f0d
Walter Agazzi <walter.agazzi@protonmail.com>
2023-12-01 13:51:47 +0100

    FALLOUT2: Add proto_copy_proto
    From upstream a4daffa30e1636df56a4a96947670db95ee379bc

--- engines/fallout2/proto.h
+++ engines/fallout2/proto.h
@@ -120,6 +120,7 @@ int objectDataWrite(Object *obj, Common::OutSaveFile *stream);
 int _proto_update_init(Object *obj);
 int _proto_dude_update_gender();
 int _proto_dude_init(const char *path);
+int proto_copy_proto(int srcPid, int dstPid);
 bool proto_is_subtype(Proto *proto, int subtype);
 int protoGetDataMember(int pid, int member, ProtoDataMemberValue *value);
 int protoInit();
