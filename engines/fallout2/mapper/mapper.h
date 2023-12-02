b02c8d7c62cd6bbe90cb9caefb410a9c2b280218
Walter Agazzi <walter.agazzi@protonmail.com>
2023-12-02 13:17:29 +0100

    FALLOUT2: Add mapper_inven_unwield
    From upstream 1366b050b081cffd506605927da4a066c77e696e

--- engines/fallout2/mapper/mapper.h
+++ engines/fallout2/mapper/mapper.h
@@ -2,10 +2,13 @@
 #define FALLOUT2_MAPPER_H
 
 #include "fallout2/map.h"
+#include "fallout2/obj_types.h"
 
 namespace Fallout2 {
 
 extern MapTransition mapInfo;
+
+int mapper_inven_unwield(Object *obj, int right_hand);
 
 } // namespace Fallout2
 
