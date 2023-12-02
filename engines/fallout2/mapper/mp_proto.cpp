e481eaf780f6296671c59b8e4e58911759d57f60
Walter Agazzi <walter.agazzi@protonmail.com>
2023-12-02 19:10:39 +0100

    FALLOUT2: Add proto_user_is_librarian
    From upstream 4eaea0e3c096d2126946dc26e2d1d2ce1b2041a8

---
+++ engines/fallout2/mapper/mp_proto.cpp
@@ -0,0 +1,8 @@
+#include "fallout2/mapper/mp_proto.h"
+
+namespace Fallout2 {
+
+// 0x559C60
+bool can_modify_protos = false;
+
+} // namespace Fallout2
---
+++ engines/fallout2/mapper/mp_proto.h
@@ -0,0 +1,10 @@
+#ifndef FALLOUT2_MAPPER_MP_PROTO_H
+#define FALLOUT2_MAPPER_MP_PROTO_H
+
+namespace Fallout2 {
+
+extern bool can_modify_protos;
+
+} // namespace Fallout2
+
+#endif
