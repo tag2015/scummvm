34a36f1e5837db2c46284421450c28042076f6b6
Walter Agazzi <walter.agazzi@protonmail.com>
2023-12-04 00:04:48 +0100

    FALLOUT2: Add mapper_edit_init
    From upstream 4cd40b33afc0fd0e7e4af85a8dd5729d5ea67a19

---
+++ engines/fallout2/mapper/map_func.cpp
@@ -0,0 +1,10 @@
+#include "fallout2/mapper/map_func.h"
+
+namespace Fallout2 {
+
+// 0x4825B0
+void setup_map_dirs() {
+	// TODO: Incomplete.
+}
+
+} // namespace Fallout2
---
+++ engines/fallout2/mapper/map_func.h
@@ -0,0 +1,10 @@
+#ifndef FALLOUT2_MAPPER_MAP_FUNC_H
+#define FALLOUT2_MAPPER_MAP_FUNC_H
+
+namespace Fallout2 {
+
+void setup_map_dirs();
+
+} // namespace Fallout2
+
+#endif
