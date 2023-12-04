a8f542ba7d2bca6c81ce443ea387b3e84033cbde
Walter Agazzi <walter.agazzi@protonmail.com>
2023-12-04 15:33:13 +0100

    FALLOUT2: Add mp_pick_kill_type
    From upstream e9f3b3488892c3056d8582dee5906822db849ebf

--- engines/fallout2/mapper/mp_proto.cpp
+++ engines/fallout2/mapper/mp_proto.cpp
@@ -4,6 +4,7 @@
 
 #include "fallout2/color.h"
 #include "fallout2/combat_ai.h"
+#include "fallout2/critter.h"
 #include "fallout2/memory.h"
 #include "fallout2/window_manager_private.h"
 
@@ -12,9 +13,29 @@ namespace Fallout2 {
 // 0x559C60
 bool can_modify_protos = false;
 
+static int mp_pick_kill_type();
+
 // 0x4922F8
 void init_mapper_protos() {
 	// TODO: Incomplete.
+}
+
+// 0x497520
+int mp_pick_kill_type() {
+	char *names[KILL_TYPE_COUNT];
+	int index;
+
+	for (index = 0; index < KILL_TYPE_COUNT; index++) {
+		names[index] = killTypeGetName(index);
+	}
+
+	return _win_list_select("Kill Type",
+							names,
+							KILL_TYPE_COUNT,
+							NULL,
+							50,
+							100,
+							_colorTable[15855]);
 }
 
 // 0x497568
