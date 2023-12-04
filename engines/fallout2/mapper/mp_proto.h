2d2d0f97f048c81f30a108777fe5a8f38b7cdf58
Walter Agazzi <walter.agazzi@protonmail.com>
2023-12-04 15:53:48 +0100

    FALLOUT2: Add proto_wall_light_str
    From upstream 6ce6639f10edaf11b7bfc65626701f4467f6d2e2

--- engines/fallout2/mapper/mp_proto.h
+++ engines/fallout2/mapper/mp_proto.h
@@ -6,6 +6,7 @@ namespace Fallout2 {
 extern bool can_modify_protos;
 
 void init_mapper_protos();
+const char *proto_wall_light_str(int flags);
 int proto_pick_ai_packet(int *value);
 
 } // namespace Fallout2
