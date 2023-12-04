23bd2cd5db3b5cc91506b0c06b9e5cda830f3e3f
Walter Agazzi <walter.agazzi@protonmail.com>
2023-12-04 15:30:53 +0100

    FALLOUT2: Add proto_pick_ai_packet
    From upstream c1258cbb4c7957cdbaa2fd6b5925bc4aea1d0360

--- engines/fallout2/mapper/mp_proto.cpp
+++ engines/fallout2/mapper/mp_proto.cpp
@@ -1,5 +1,12 @@
 #include "fallout2/mapper/mp_proto.h"
 
+// #include <string.h>
+
+#include "fallout2/color.h"
+#include "fallout2/combat_ai.h"
+#include "fallout2/memory.h"
+#include "fallout2/window_manager_private.h"
+
 namespace Fallout2 {
 
 // 0x559C60
@@ -8,6 +15,43 @@ bool can_modify_protos = false;
 // 0x4922F8
 void init_mapper_protos() {
 	// TODO: Incomplete.
+}
+
+// 0x497568
+int proto_pick_ai_packet(int *value) {
+	int count;
+	char **names;
+	int index;
+	int rc;
+
+	count = combat_ai_num();
+	if (count <= 0) {
+		return -1;
+	}
+
+	names = (char **)internal_malloc(sizeof(char *) * count);
+	for (index = 0; index < count; index++) {
+		names[index] = (char *)internal_malloc(strlen(combat_ai_name(index)) + 1);
+		strcpy(names[index], combat_ai_name(index));
+	}
+
+	rc = _win_list_select("AI Packet",
+						  names,
+						  count,
+						  NULL,
+						  50,
+						  100,
+						  _colorTable[15855]);
+	if (rc != -1) {
+		*value = rc;
+	}
+
+	for (index = 0; index < count; index++) {
+		internal_free(names[index]);
+	}
+
+	internal_free(names);
+	return 0;
 }
 
 } // namespace Fallout2
