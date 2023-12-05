4de558477bcdb430036335cb27d94e726cb01097
Walter Agazzi <walter.agazzi@protonmail.com>
2023-12-05 20:37:20 +0100

    FALLOUT2: Add update_toolname
    From upstream ee1923866729e13f25aaa695edfb6a88b9e4a9e9

--- engines/fallout2/proto.cpp
+++ engines/fallout2/proto.cpp
@@ -171,7 +171,7 @@ char *_proto_none_str;
 static char *gBodyTypeNames[BODY_TYPE_COUNT];
 
 // 0x664834
-static char *gItemTypeNames[ITEM_TYPE_COUNT];
+char *gItemTypeNames[ITEM_TYPE_COUNT];
 
 // 0x66484C
 static char *gDamageTypeNames[DAMAGE_TYPE_COUNT];
