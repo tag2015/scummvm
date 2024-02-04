8ffceeae4a08b2ad663ac220828ceff2ac877f48
Walter Agazzi <walter.agazzi@protonmail.com>
2024-02-04 15:29:53 +0100

    FALLOUT2: Cleanup win32, remove platform specific stuff

--- engines/fallout2/win32.cpp
+++ engines/fallout2/win32.cpp
@@ -2,74 +2,9 @@
 
 #include "fallout2/win32.h"
 
-/*#include <stdlib.h>
-
-#include <SDL.h>
-
-#ifndef _WIN32
-#include <unistd.h>
-#endif
-
-#include "main.h"
-#include "svga.h"
-#include "window_manager.h"
-
-#if __APPLE__ && TARGET_OS_IOS
-#include "platform/ios/paths.h"
-#endif*/
-
 namespace Fallout2 {
 
-#if 0
-#ifdef _WIN32
 // 0x51E444
-bool gProgramIsActive = false;
-
-// GNW95MUTEX
-HANDLE _GNW95_mutex = nullptr;
-
-// 0x4DE700
-int main(int argc, char* argv[]) {
-	_GNW95_mutex = CreateMutexA(0, TRUE, "GNW95MUTEX");
-	if (GetLastError() == ERROR_SUCCESS) {
-		SDL_ShowCursor(SDL_DISABLE);
-
-		gProgramIsActive = true;
-		falloutMain(argc, argv);
-
-		CloseHandle(_GNW95_mutex);
-	}
-	return 0;
-}
-#else
-bool gProgramIsActive = false;
-
-int main(int argc, char* argv[]) {
-#if __APPLE__ && TARGET_OS_IOS
-	SDL_SetHint(SDL_HINT_MOUSE_TOUCH_EVENTS, "0");
-	SDL_SetHint(SDL_HINT_TOUCH_MOUSE_EVENTS, "0");
-	chdir(iOSGetDocumentsPath());
-#endif
-
-#if __APPLE__ && TARGET_OS_OSX
-	char* basePath = SDL_GetBasePath();
-	chdir(basePath);
-	SDL_free(basePath);
-#endif
-
-#if __ANDROID__
-	SDL_SetHint(SDL_HINT_MOUSE_TOUCH_EVENTS, "0");
-	SDL_SetHint(SDL_HINT_TOUCH_MOUSE_EVENTS, "0");
-	chdir(SDL_AndroidGetExternalStoragePath());
-#endif
-
-	SDL_ShowCursor(SDL_DISABLE);
-	gProgramIsActive = true;
-	return falloutMain(argc, argv);
-}
-#endif
-#endif
-
 bool gProgramIsActive = true;
 
 } // namespace Fallout2
