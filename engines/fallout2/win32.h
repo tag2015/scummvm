1086ef1f487c05283cd83f89ca7c49c626d29ba1
Walter Agazzi <walter.agazzi@protonmail.com>
2024-02-04 15:29:53 +0100

    FALLOUT2: Cleanup win32, remove platform specific stuff

--- engines/fallout2/win32.h
+++ engines/fallout2/win32.h
@@ -1,18 +1,9 @@
 #ifndef FALLOUT2_WIN32_H
 #define FALLOUT2_WIN32_H
 
-/*#ifdef _WIN32
-#include <windows.h>
-#endif*/
-
 namespace Fallout2 {
 
-// #ifdef _WIN32
 extern bool gProgramIsActive;
-// extern HANDLE _GNW95_mutex;
-// #else
-
-// bool gProgramIsActive;
 
 } // namespace Fallout2
 
