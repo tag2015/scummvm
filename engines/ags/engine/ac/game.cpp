tag2015/scummvm

commit d65935c2761ddf89d9e4270a61100cf09bdf0633

AGS: Engine: store DoOnceOnly tokens in a unordered_set
From upstream c3927ec4b225778eba98d9ab0eb1f785eb3e6c03

engines/ags/engine/ac/game.cpp
@@ -353,7 +353,7 @@ bool do_save_game_dialog() {
 }
 
 void free_do_once_tokens() {
-	_GP(play).do_once_tokens.resize(0);
+	_GP(play).do_once_tokens.clear();
 }
 
 
@@ -534,12 +534,10 @@ ScriptViewFrame *Game_GetViewFrame(int viewNumber, int loopNumber, int frame) {
 }
 
 int Game_DoOnceOnly(const char *token) {
-	for (int i = 0; i < (int)_GP(play).do_once_tokens.size(); i++) {
-		if (_GP(play).do_once_tokens[i] == token) {
-			return 0;
-		}
-	}
-	_GP(play).do_once_tokens.push_back(token);
+	if (_GP(play).do_once_tokens.count(String::Wrapper(token)) > 0)
+		return 0;
+	_GP(play).do_once_tokens.insert(token);
+
 	return 1;
 }
 

engines/ags/engine/ac/game_state.cpp
@@ -617,11 +617,10 @@ void GameState::ReadFromSavegame(Shared::Stream *in, GameDataVersion data_ver, G
 		in->ReadInt32(); // gui_draw_order
 		in->ReadInt32(); // do_once_tokens;
 	}
-	int num_do_once_tokens = in->ReadInt32();
-	do_once_tokens.resize(num_do_once_tokens);
+	r_data.DoOnceCount = static_cast<uint32_t>(in->ReadInt32());
 	if (!old_save) {
-		for (int i = 0; i < num_do_once_tokens; ++i) {
-			StrUtil::ReadString(do_once_tokens[i], in);
+		for (size_t i = 0; i < r_data.DoOnceCount; ++i) {
+			do_once_tokens.insert(StrUtil::ReadString(in));
 		}
 	}
 	text_min_display_time_ms = in->ReadInt32();
@@ -805,9 +804,9 @@ void GameState::WriteForSavegame(Shared::Stream *out) const {
 	out->WriteInt32(gamma_adjustment);
 	out->WriteInt16(temporarily_turned_off_character);
 	out->WriteInt16(inv_backwards_compatibility);
-	out->WriteInt32(do_once_tokens.size());
-	for (int i = 0; i < (int)do_once_tokens.size(); ++i) {
-		StrUtil::WriteString(do_once_tokens[i], out);
+	out->WriteInt32(static_cast<uint32_t>(do_once_tokens.size()));
+	for (const auto &token : do_once_tokens) {
+		StrUtil::WriteString(token, out);
 	}
 	out->WriteInt32(text_min_display_time_ms);
 	out->WriteInt32(ignore_user_input_after_text_timeout_ms);

engines/ags/engine/ac/game_state.h
@@ -23,6 +23,7 @@
 #define AGS_ENGINE_AC_GAME_STATE_H
 
 #include "common/std/memory.h"
+#include "common/std/unordered_set.h"
 #include "common/std/vector.h"
 #include "ags/shared/ac/character_info.h"
 #include "ags/shared/ac/keycode.h"
@@ -229,7 +230,7 @@ struct GameState {
 	short temporarily_turned_off_character = 0;  // Hide Player Charactr ticked
 	short inv_backwards_compatibility = 0;  // tells to use legacy inv_* variables
 	std::vector<int> gui_draw_order; // used only for hit detection now
-	std::vector<AGS::Shared::String> do_once_tokens;
+	std::unordered_set<AGS::Shared::String> do_once_tokens;
 	int   text_min_display_time_ms = 0;
 	int   ignore_user_input_after_text_timeout_ms = 0;
 	int32_t default_audio_type_volumes[MAX_AUDIO_TYPES];

engines/ags/engine/game/savegame.cpp
@@ -104,6 +104,7 @@ RestoredData::ScriptData::ScriptData()
 
 RestoredData::RestoredData()
 	: FPS(0)
+	, DoOnceCount(0u)
 	, RoomVolume(kRoomVolumeNormal)
 	, CursorID(0)
 	, CursorMode(0) {

engines/ags/engine/game/savegame_internal.h
@@ -81,6 +81,8 @@ struct RestoredData {
 	};
 	ScriptData              GlobalScript;
 	std::vector<ScriptData> ScriptModules;
+	// Game state data (loaded ahead)
+	uint32_t				DoOnceCount;
 	// Room data (has to be be preserved until room is loaded)
 	PBitmap                 RoomBkgScene[MAX_ROOM_BGFRAMES];
 	int16_t                 RoomLightLevels[MAX_ROOM_REGIONS];

engines/ags/engine/game/savegame_v321.cpp
@@ -179,16 +179,6 @@ static void ReadGameState_Aligned(Stream *in, GameDataVersion data_ver, Restored
 	_GP(play).ReadFromSavegame(&align_s, data_ver, kGSSvgVersion_OldFormat, r_data);
 }
 
-static void restore_game_play_ex_data(Stream *in) {
-	char rbuffer[200];
-	for (size_t i = 0; i < _GP(play).do_once_tokens.size(); ++i) {
-		StrUtil::ReadCStr(rbuffer, in, sizeof(rbuffer));
-		_GP(play).do_once_tokens[i] = rbuffer;
-	}
-
-	in->Seek(_GP(game).numgui * sizeof(int32_t)); // gui_draw_order
-}
-
 static void restore_game_play(Stream *in, GameDataVersion data_ver, RestoredData &r_data) {
 	int screenfadedout_was = _GP(play).screen_is_faded_out;
 	int roomchanges_was = _GP(play).room_changes;
@@ -199,7 +189,14 @@ static void restore_game_play(Stream *in, GameDataVersion data_ver, RestoredData
 	_GP(play).screen_is_faded_out = screenfadedout_was;
 	_GP(play).room_changes = roomchanges_was;
 
-	restore_game_play_ex_data(in);
+	char rbuffer[200]; // old doonceonly token length
+	for (size_t i = 0; i < r_data.DoOnceCount; ++i) {
+		StrUtil::ReadCStr(rbuffer, in, sizeof(rbuffer));
+		_GP(play).do_once_tokens.insert(rbuffer);
+	}
+
+	// Skip gui_draw_order (no longer applied from saves)
+	in->Seek(_GP(game).numgui * sizeof(int32_t));
 }
 
 static void ReadMoveList_Aligned(Stream *in) {

engines/ags/engine/main/engine.cpp
@@ -651,7 +651,6 @@ void engine_init_game_settings() {
 	_GP(play).temporarily_turned_off_character = -1;
 	_GP(play).inv_backwards_compatibility = 0;
 	_GP(play).gamma_adjustment = 100;
-	_GP(play).do_once_tokens.resize(0);
 	_GP(play).music_queue_size = 0;
 	_GP(play).shakesc_length = 0;
 	_GP(play).wait_counter = 0;

commit 5e00dea16bc123d94a7b729741d0277142de2a1d

AGS: Engine: Fix DoOnceOnly not finding any tokens
Was broken in d65935c2761ddf89d9e4270a61100cf09bdf0633

engines/ags/engine/ac/game.cpp
@@ -543,10 +543,10 @@ ScriptViewFrame *Game_GetViewFrame(int view, int loop, int frame) {
 }
 
 int Game_DoOnceOnly(const char *token) {
-	if (_GP(play).do_once_tokens.count(String::Wrapper(token)) > 0)
+	if (_GP(play).do_once_tokens.find(String::Wrapper(token)) != _GP(play).do_once_tokens.end())
 		return 0;
-	_GP(play).do_once_tokens.insert(token);
 
+	_GP(play).do_once_tokens.insert(token);
 	return 1;
 }
 