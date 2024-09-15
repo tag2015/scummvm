tag2015/scummvm

commit 54f8b2bccec35b2c127204e6bebef674ffb6a740

AGS: Engine: store ScriptString in memory similarly to dynamic array
This potentially allows to access any metadata while passing string pointer around the script functions.

As a consequence: never wrap external buffer, always allocate your own.
From upstream 043758b1bb5116f038c5c597d17fe9524b92b3c2

engines/ags/engine/ac/string.cpp
@@ -42,6 +42,14 @@
 
 namespace AGS3 {
 
+const char *CreateNewScriptString(const char *text) {
+	return (const char *)ScriptString::Create(text).Obj;
+}
+
+char *CreateNewScriptString(size_t buf_length) {
+	return (char *)ScriptString::Create(buf_length).Obj;
+}
+
 int String_IsNullOrEmpty(const char *thisString) {
 	if ((thisString == nullptr) || (thisString[0] == 0))
 		return 1;
@@ -54,20 +62,20 @@ const char *String_Copy(const char *srcString) {
 }
 
 const char *String_Append(const char *thisString, const char *extrabit) {
-	size_t ln = strlen(thisString) + strlen(extrabit) + 1;
-	char *buffer = (char *)malloc(ln);
-	Common::strcpy_s(buffer, ln, thisString);
-	Common::strcat_s(buffer, ln, extrabit);
-	return CreateNewScriptString(buffer, false);
+	size_t new_len = strlen(thisString) + strlen(extrabit);
+	char *buffer = CreateNewScriptString(new_len);
+	Common::strcpy_s(buffer, new_len, thisString);
+	Common::strcat_s(buffer, new_len, extrabit);
+	return buffer;
 }
 
 const char *String_AppendChar(const char *thisString, int extraOne) {
 	char chr[5]{};
 	size_t chw = usetc(chr, extraOne);
-	size_t ln = strlen(thisString) + chw + 1;
-	char *buffer = (char *)malloc(ln);
-	Common::sprintf_s(buffer, ln, "%s%s", thisString, chr);
-	return CreateNewScriptString(buffer, false);
+    size_t new_len = strlen(thisString) + chw;
+    char *buffer = CreateNewScriptString(new_len);
+	Common::sprintf_s(buffer, new_len, "%s%s", thisString, chr);
+	return buffer;
 }
 
 const char *String_ReplaceCharAt(const char *thisString, int index, int newChar) {
@@ -81,12 +89,12 @@ const char *String_ReplaceCharAt(const char *thisString, int index, int newChar)
 	size_t old_sz = ucwidth(uchar);
 	char new_chr[5]{};
 	size_t new_chw = usetc(new_chr, newChar);
-	size_t total_sz = off + remain_sz + new_chw - old_sz + 1;
-	char *buffer = (char *)malloc(total_sz);
+	size_t new_len = off + remain_sz + new_chw - old_sz;
+	char *buffer = CreateNewScriptString(new_len);
 	memcpy(buffer, thisString, off);
 	memcpy(buffer + off, new_chr, new_chw);
 	memcpy(buffer + off + new_chw, thisString + off + old_sz, remain_sz - old_sz + 1);
-	return CreateNewScriptString(buffer, false);
+	return buffer;
 }
 
 const char *String_Truncate(const char *thisString, int length) {
@@ -97,10 +105,10 @@ const char *String_Truncate(const char *thisString, int length) {
 		return thisString;
 
 	size_t sz = uoffset(thisString, length);
-	char *buffer = (char *)malloc(sz + 1);
+	char *buffer = CreateNewScriptString(sz);
 	memcpy(buffer, thisString, sz);
 	buffer[sz] = 0;
-	return CreateNewScriptString(buffer, false);
+	return buffer;
 }
 
 const char *String_Substring(const char *thisString, int index, int length) {
@@ -114,10 +122,10 @@ const char *String_Substring(const char *thisString, int index, int length) {
 	size_t end = uoffset(thisString + start, sublen) + start;
 	size_t copysz = end - start;
 
-	char *buffer = (char *)malloc(copysz + 1);
+	char *buffer = CreateNewScriptString(copysz);
 	memcpy(buffer, thisString + start, copysz);
 	buffer[copysz] = 0;
-	return CreateNewScriptString(buffer, false);
+	return buffer;
 }
 
 int String_CompareTo(const char *thisString, const char *otherString, bool caseSensitive) {
@@ -185,19 +193,23 @@ const char *String_Replace(const char *thisString, const char *lookForText, cons
 	}
 
 	resultBuffer[outputSize] = 0; // terminate
-	return CreateNewScriptString(resultBuffer, true);
+	return CreateNewScriptString(resultBuffer);
 }
 
 const char *String_LowerCase(const char *thisString) {
-	char *buffer = ags_strdup(thisString);
+	size_t len = strlen(thisString);
+	char *buffer = CreateNewScriptString(len);
+	memcpy(buffer, thisString, len);
 	ustrlwr(buffer);
-	return CreateNewScriptString(buffer, false);
+	return buffer;
 }
 
 const char *String_UpperCase(const char *thisString) {
-	char *buffer = ags_strdup(thisString);
+	size_t len = strlen(thisString);
+	char *buffer = CreateNewScriptString(len);
+	memcpy(buffer, thisString, len);
 	ustrupr(buffer);
-	return CreateNewScriptString(buffer, false);
+	return buffer;
 }
 
 int String_GetChars(const char *texx, int index) {
@@ -244,34 +256,6 @@ int StrContains(const char *s1, const char *s2) {
 }
 
 //=============================================================================
-
-const char *CreateNewScriptString(const String &fromText) {
-	return (const char *)CreateNewScriptStringObj(fromText.GetCStr(), true).Obj;
-}
-
-const char *CreateNewScriptString(const char *fromText, bool reAllocate) {
-	return (const char *)CreateNewScriptStringObj(fromText, reAllocate).Obj;
-}
-
-DynObjectRef CreateNewScriptStringObj(const String &fromText) {
-	return CreateNewScriptStringObj(fromText.GetCStr(), true);
-}
-
-DynObjectRef CreateNewScriptStringObj(const char *fromText, bool reAllocate) {
-	ScriptString *str;
-	if (reAllocate) {
-		str = new ScriptString(fromText);
-	} else { // TODO: refactor to avoid const casts!
-		str = new ScriptString(const_cast<char *>(fromText), true);
-	}
-	void *obj_ptr = str->GetTextPtr();
-	int32_t handle = ccRegisterManagedObject(obj_ptr, str);
-	if (handle == 0) {
-		delete str;
-		return DynObjectRef();
-	}
-	return DynObjectRef(handle, obj_ptr, str);
-}
 
 size_t break_up_text_into_lines(const char *todis, bool apply_direction, SplitLines &lines, int wii, int fonnt, size_t max_lines) {
 	if (fonnt == -1)

engines/ags/engine/ac/dynobj/cc_dynamic_array.cpp
@@ -21,7 +21,7 @@
 
 #include "ags/engine/ac/dynobj/cc_dynamic_array.h"
 #include "ags/engine/ac/dynobj/dynobj_manager.h"
-#include "ags/shared/util/memory_stream.h"
+#include "ags/engine/ac/dynobj/script_string.h"
 #include "ags/globals.h"
 
 namespace AGS3 {
@@ -62,7 +62,7 @@ size_t CCDynamicArray::CalcSerializeSize(const void *address) {
 	return hdr.TotalSize + FileHeaderSz;
 }
 
-void CCDynamicArray::Serialize(const void *address, AGS::Shared::Stream *out) {
+void CCDynamicArray::Serialize(const void *address, Stream *out) {
 	const Header &hdr = GetHeader(address);
 	out->WriteInt32(hdr.ElemCount);
 	out->WriteInt32(hdr.TotalSize);
@@ -101,7 +101,7 @@ DynObjectRef DynamicArrayHelpers::CreateStringArray(const std::vector<const char
 	// Create script strings and put handles into array
 	int32_t *slots = static_cast<int32_t *>(arr.Obj);
 	for (auto s : items) {
-		DynObjectRef str = _G(stringClassImpl)->CreateString(s);
+		DynObjectRef str = ScriptString::Create(s);
 		// We must add reference count, because the string is going to be saved
 		// within another object (array), not returned to script directly
 		ccAddObjectReference(str.Handle);

engines/ags/engine/ac/dynobj/cc_script_object.h
@@ -119,12 +119,6 @@ struct ICCObjectReader {
 	virtual void Unserialize(int32_t handle, const char *serializedData, int dataSize) = 0;
 };
 
-// The interface of a dynamic String allocator.
-struct ICCStringClass {
-	virtual ~ICCStringClass() {}
-	virtual DynObjectRef CreateString(const char *fromText) = 0;
-};
-
 } // namespace AGS3
 
 #endif

engines/ags/engine/ac/dynobj/cc_serializer.cpp
@@ -82,8 +82,7 @@ void AGSDeSerializer::Unserialize(int index, const char *objectType, const char 
 	} else if (strcmp(objectType, "Object") == 0) {
 		_GP(ccDynamicObject).Unserialize(index, &mems, data_sz);
 	} else if (strcmp(objectType, "String") == 0) {
-		ScriptString *scf = new ScriptString();
-		scf->Unserialize(index, &mems, data_sz);
+		_GP(myScriptStringImpl).Unserialize(index, &mems, data_sz);
 	} else if (strcmp(objectType, "File") == 0) {
 		// files cannot be restored properly -- so just recreate
 		// the object; attempting any operations on it will fail

engines/ags/engine/ac/dynobj/dynobj_manager.cpp
@@ -46,11 +46,6 @@ namespace AGS3 {
 
 using namespace AGS::Shared;
 
-// set the class that will be used for dynamic strings
-void ccSetStringClassImpl(ICCStringClass *theClass) {
-	_G(stringClassImpl) = theClass;
-}
-
 // register a memory handle for the object and allow script
 // pointers to point to it
 int32_t ccRegisterManagedObject(void *object, IScriptObject *callback, ScriptValueType obj_type) {

engines/ags/engine/ac/dynobj/dynobj_manager.h
@@ -45,8 +45,6 @@ class Stream;
 
 using namespace AGS; // FIXME later
 
-// set the class that will be used for dynamic strings
-extern void  ccSetStringClassImpl(ICCStringClass *theClass);
 // register a memory handle for the object and allow script
 // pointers to point to it
 extern int32_t ccRegisterManagedObject(void *object, IScriptObject *, ScriptValueType obj_type = kScValScriptObject);

engines/ags/engine/ac/dynobj/script_string.cpp
@@ -23,61 +23,57 @@
 #include "ags/engine/ac/dynobj/dynobj_manager.h"
 #include "ags/engine/ac/string.h"
 #include "ags/shared/util/stream.h"
+#include "ags/globals.h"
 
 namespace AGS3 {
 
 using namespace AGS::Shared;
 
-DynObjectRef ScriptString::CreateString(const char *fromText) {
-	return CreateNewScriptStringObj(fromText);
-}
-
-int ScriptString::Dispose(void * /*address*/, bool force) {
-	// always dispose
-	if (_text) {
-		free(_text);
-		_text = nullptr;
-	}
-	delete this;
-	return 1;
-}
-
 const char *ScriptString::GetType() {
 	return "String";
 }
 
-size_t ScriptString::CalcSerializeSize(const void * /*address*/) {
-	return _len + 1 + sizeof(int32_t);
+int ScriptString::Dispose(void *address, bool /*force*/) {
+	delete[] (static_cast<uint8_t *>(address) - MemHeaderSz);
+	return 1;
 }
 
-void ScriptString::Serialize(const void * /*address*/, Stream *out) {
-	const auto *cstr = _text ? _text : "";
-	out->WriteInt32(_len);
-	out->Write(cstr, _len + 1);
+size_t ScriptString::CalcSerializeSize(const void *address) {
+	const Header &hdr = GetHeader(address);
+	return hdr.Length + 1 + FileHeaderSz;
+}
+
+void ScriptString::Serialize(const void *address, Stream *out) {
+	const Header &hdr = GetHeader(address);
+	out->WriteInt32(hdr.Length);
+	out->Write(address, hdr.Length + 1); // it was writing trailing 0 for some reason
 }
 
 void ScriptString::Unserialize(int index, Stream *in, size_t /*data_sz*/) {
-	_len = in->ReadInt32();
-	_text = (char *)malloc(_len + 1);
-	in->Read(_text, _len + 1);
-	_text[_len] = 0; // for safety
-	ccRegisterUnserializedObject(index, _text, this);
+	size_t len = in->ReadInt32();
+	uint8_t *buf = new uint8_t[len + 1 + MemHeaderSz];
+	Header &hdr = reinterpret_cast<Header &>(*buf);
+	hdr.Length = len;
+	char *text_ptr = reinterpret_cast<char *>(buf + MemHeaderSz);
+	in->Read(text_ptr, len + 1); // it was writing trailing 0 for some reason
+	text_ptr[len] = 0;           // for safety
+	ccRegisterUnserializedObject(index, text_ptr, this);
 }
 
-ScriptString::ScriptString(const char *text) {
-	_len = strlen(text);
-	_text = (char *)malloc(_len + 1);
-	memcpy(_text, text, _len + 1);
-}
-
-ScriptString::ScriptString(char *text, bool take_ownership) {
-	_len = strlen(text);
-	if (take_ownership) {
-		_text = text;
-	} else {
-		_text = (char *)malloc(_len + 1);
-		memcpy(_text, text, _len + 1);
+DynObjectRef ScriptString::CreateImpl(const char *text, size_t buf_len) {
+	size_t len = text ? strlen(text) : buf_len;
+	uint8_t *buf = new uint8_t[len + 1 + MemHeaderSz];
+	Header &hdr = reinterpret_cast<Header &>(*buf);
+	hdr.Length = len;
+	char *text_ptr = reinterpret_cast<char *>(buf + MemHeaderSz);
+	if (text)
+		memcpy(text_ptr, text, len + 1);
+	int32_t handle = ccRegisterManagedObject(text_ptr, &_GP(myScriptStringImpl));
+	if (handle == 0) {
+		delete[] buf;
+		return DynObjectRef();
 	}
+	return DynObjectRef(handle, text_ptr, &_GP(myScriptStringImpl));
 }
 
 } // namespace AGS3

engines/ags/engine/ac/dynobj/script_string.h
@@ -26,31 +26,41 @@
 
 namespace AGS3 {
 
-struct ScriptString final : AGSCCDynamicObject, ICCStringClass {
-	int Dispose(void *address, bool force) override;
-	const char *GetType() override;
-	void Unserialize(int index, AGS::Shared::Stream *in, size_t data_sz) override;
-
-	DynObjectRef CreateString(const char *fromText) override;
+struct ScriptString final : AGSCCDynamicObject {
+public:
+	struct Header {
+		uint32_t Length = 0u;
+	};
 
 	ScriptString() = default;
-	ScriptString(const char *text);
-	ScriptString(char *text, bool take_ownership);
-	char *GetTextPtr() const {
-		return _text;
+	~ScriptString() = default;
+
+	inline static const Header &GetHeader(const void *address) {
+		return reinterpret_cast<const Header &>(*(static_cast<const uint8_t *>(address) - MemHeaderSz));
 	}
 
-protected:
+	// Create a new script string by copying the given text
+	static DynObjectRef Create(const char *text) { return CreateImpl(text, -1); }
+	// Create a new script string with a buffer of at least the given text length
+	static DynObjectRef Create(size_t buf_len) { return CreateImpl(nullptr, buf_len); }
+
+	const char *GetType() override;
+	int Dispose(void *address, bool force) override;
+	void Unserialize(int index, AGS::Shared::Stream *in, size_t data_sz) override;
+
+private:
+	// The size of the array's header in memory, prepended to the element data
+	static const size_t MemHeaderSz = sizeof(Header);
+	// The size of the serialized header
+	static const size_t FileHeaderSz = sizeof(uint32_t);
+
+	static DynObjectRef CreateImpl(const char *text, size_t buf_len);
+
+	// Savegame serialization
 	// Calculate and return required space for serialization, in bytes
 	size_t CalcSerializeSize(const void *address) override;
 	// Write object data into the provided stream
 	void Serialize(const void *address, AGS::Shared::Stream *out) override;
-
-private:
-	// TODO: the preallocated text buffer may be assigned externally;
-	// find out if it's possible to refactor while keeping same functionality
-	char *_text = nullptr;
-	size_t _len = 0;
 };
 
 } // namespace AGS3

commit aa50eb4499a322071442181e8da178652fd939ee

AGS: Fix improper string length calculation in string functions
Broken in 54f8b2bccec35b2c127204e6bebef674ffb6a740

engines/ags/engine/ac/string.cpp
@@ -62,7 +62,7 @@ const char *String_Copy(const char *srcString) {
 }
 
 const char *String_Append(const char *thisString, const char *extrabit) {
-	size_t new_len = strlen(thisString) + strlen(extrabit);
+	size_t new_len = strlen(thisString) + strlen(extrabit) + 1;
 	char *buffer = CreateNewScriptString(new_len);
 	Common::strcpy_s(buffer, new_len, thisString);
 	Common::strcat_s(buffer, new_len, extrabit);
@@ -72,7 +72,7 @@ const char *String_Append(const char *thisString, const char *extrabit) {
 const char *String_AppendChar(const char *thisString, int extraOne) {
 	char chr[5]{};
 	size_t chw = usetc(chr, extraOne);
-    size_t new_len = strlen(thisString) + chw;
+    size_t new_len = strlen(thisString) + chw + 1;
     char *buffer = CreateNewScriptString(new_len);
 	Common::sprintf_s(buffer, new_len, "%s%s", thisString, chr);
 	return buffer;
@@ -89,7 +89,7 @@ const char *String_ReplaceCharAt(const char *thisString, int index, int newChar)
 	size_t old_sz = ucwidth(uchar);
 	char new_chr[5]{};
 	size_t new_chw = usetc(new_chr, newChar);
-	size_t new_len = off + remain_sz + new_chw - old_sz;
+	size_t new_len = off + remain_sz + new_chw - old_sz + 1;
 	char *buffer = CreateNewScriptString(new_len);
 	memcpy(buffer, thisString, off);
 	memcpy(buffer + off, new_chr, new_chw);
@@ -105,7 +105,7 @@ const char *String_Truncate(const char *thisString, int length) {
 		return thisString;
 
 	size_t sz = uoffset(thisString, length);
-	char *buffer = CreateNewScriptString(sz);
+	char *buffer = CreateNewScriptString(sz + 1);
 	memcpy(buffer, thisString, sz);
 	buffer[sz] = 0;
 	return buffer;
@@ -122,7 +122,7 @@ const char *String_Substring(const char *thisString, int index, int length) {
 	size_t end = uoffset(thisString + start, sublen) + start;
 	size_t copysz = end - start;
 
-	char *buffer = CreateNewScriptString(copysz);
+	char *buffer = CreateNewScriptString(copysz + 1);
 	memcpy(buffer, thisString + start, copysz);
 	buffer[copysz] = 0;
 	return buffer;
@@ -198,16 +198,16 @@ const char *String_Replace(const char *thisString, const char *lookForText, cons
 
 const char *String_LowerCase(const char *thisString) {
 	size_t len = strlen(thisString);
-	char *buffer = CreateNewScriptString(len);
-	memcpy(buffer, thisString, len);
+	char *buffer = CreateNewScriptString(len + 1);
+	memcpy(buffer, thisString, len + 1);
 	ustrlwr(buffer);
 	return buffer;
 }
 
 const char *String_UpperCase(const char *thisString) {
 	size_t len = strlen(thisString);
-	char *buffer = CreateNewScriptString(len);
-	memcpy(buffer, thisString, len);
+	char *buffer = CreateNewScriptString(len + 1);
+	memcpy(buffer, thisString, len + 1);
 	ustrupr(buffer);
 	return buffer;
 }