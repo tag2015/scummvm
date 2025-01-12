695bc0df2df667db5d0bd1572eb9a2f234ce5ad5
Walter Agazzi <walter.agazzi@protonmail.com>
2025-01-13 00:04:24 +0100

    FALLOUT2: Cleanup debug.h

--- engines/fallout2/debug.h
+++ engines/fallout2/debug.h
@@ -5,18 +5,6 @@
 
 namespace Fallout2 {
 
-/* typedef int(DebugPrintProc)(char *string);
-
-void _GNW_debug_init();
-void _debug_register_mono();
-void _debug_register_log(const char *fileName, const char *mode);
-void _debug_register_screen();
-void _debug_register_env();
-void _debug_register_func(DebugPrintProc *proc);
-int debugPrint(const char *format, ...);
-void _debug_exit(void);
-*/
-
 template<typename... Args>
 void debugPrint(const char *format, Args... args) {
 	debug(1, format, args...);
@@ -24,4 +12,4 @@ void debugPrint(const char *format, Args... args) {
 
 } // namespace Fallout2
 
-#endif /* DEBUG_H */
+#endif
