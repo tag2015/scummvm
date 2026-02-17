42e4e9b7e9c0068b9a608bda1d9e58da0205dd19
Walter Agazzi <walter.agazzi@protonmail.com>
2026-02-17 23:48:43 +0100

    FALLOUT2: Use gCitiesLimitFix variable
    From upstream db07e95f5b4d2972f24dfb18ce175d91848d4a4c

--- engines/fallout2/worldmap.cpp
+++ engines/fallout2/worldmap.cpp
@@ -819,6 +819,7 @@ static int wmMaxEncBaseTypes;
 static int wmMaxEncounterInfoTables;
 
 static bool gTownMapHotkeysFix;
+static bool gCitiesLimitFix;
 static double gGameTimeIncRemainder = 0.0;
 static FrmImage _backgroundFrmImage;
 static FrmImage _townFrmImage;
@@ -871,6 +872,8 @@ int wmWorldMap_init() {
 	// SFALL
 	gTownMapHotkeysFix = true;
 	configGetBool(&gSfallConfig, SFALL_CONFIG_MISC_KEY, SFALL_CONFIG_TOWN_MAP_HOTKEYS_FIX_KEY, &gTownMapHotkeysFix);
+	gCitiesLimitFix = true;
+	configGetBool(&gSfallConfig, SFALL_CONFIG_MISC_KEY, SFALL_CONFIG_CITIES_LIMIT_FIX, &gCitiesLimitFix);
 
 	// CE: City size fids should be initialized during startup. They are used
 	// during |wmTeleportToArea| to calculate worldmap position when jumping
@@ -1177,9 +1180,7 @@ int wmWorldMap_load(Common::InSaveFile *stream) {
 	if (stream->err())
 		return -1;
 
-	bool isCitiesLimitFixEnabled = true;
-	configGetBool(&gSfallConfig, SFALL_CONFIG_MISC_KEY, SFALL_CONFIG_CITIES_LIMIT_FIX, &isCitiesLimitFixEnabled);
-	if (isCitiesLimitFixEnabled && numCities != wmMaxAreaNum) {
+	if (gCitiesLimitFix && numCities != wmMaxAreaNum) {
 		debugPrint("WorldMap Error: Cities limit fix is enabled, "
 				   "but the number of cities in the save file is different from "
 				   "the number of cities in the worldmap.txt file.");
@@ -2547,10 +2548,7 @@ static int wmAreaInit() {
 
 	configFree(&cfg);
 
-	bool isCitiesLimitFixEnabled = true;
-	configGetBool(&gSfallConfig, SFALL_CONFIG_MISC_KEY, SFALL_CONFIG_CITIES_LIMIT_FIX, &isCitiesLimitFixEnabled);
-
-	if (!isCitiesLimitFixEnabled && wmMaxAreaNum != CITY_COUNT) {
+	if (!gCitiesLimitFix && wmMaxAreaNum != CITY_COUNT) {
 		showMesageBox("\nwmAreaInit::Error loading Cities!");
 		error("wmAreaInit::Error loading Cities");
 	}
