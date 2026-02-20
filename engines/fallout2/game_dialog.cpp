d0fa95770c21eceda3d7abd28aea10d797f9b49c
Walter Agazzi <walter.agazzi@protonmail.com>
2026-02-20 17:31:32 +0100

    FALLOUT2: Update game_dialog.cc - fix minor layout bug
    From upstream 33dae201fe534ac374683bfac5636aa97f36abaf

--- engines/fallout2/game_dialog.cpp
+++ engines/fallout2/game_dialog.cpp
@@ -4267,6 +4267,8 @@ int _gdialog_window_create() {
 			// TODO: Not sure about offsets.
 			blitBufferToBuffer(bgWindowBuf + screenWidth * (GAME_DIALOG_WINDOW_HEIGHT - _dialogue_subwin_len), screenWidth, _dialogue_subwin_len, screenWidth, windowBuf, screenWidth);
 
+			_gdialog_buttons[0] = buttonCreate(gGameDialogWindow, 593, 41, 14, 14, -1, -1, -1, -1, _redButtonNormalFrmImage.getData(), _redButtonPressedFrmImage.getData(), nullptr, BUTTON_FLAG_TRANSPARENT);
+
 			if (_dialogue_just_started) {
 				windowRefresh(gGameDialogBackgroundWindow);
 				_gdialog_scroll_subwin(gGameDialogWindow, true, backgroundFrmData, windowBuf, nullptr, _dialogue_subwin_len, true);
@@ -4276,7 +4278,6 @@ int _gdialog_window_create() {
 			}
 
 			// BARTER/TRADE
-			_gdialog_buttons[0] = buttonCreate(gGameDialogWindow, 593, 41, 14, 14, -1, -1, -1, -1, _redButtonNormalFrmImage.getData(), _redButtonPressedFrmImage.getData(), nullptr, BUTTON_FLAG_TRANSPARENT);
 			if (_gdialog_buttons[0] != -1) {
 				buttonSetMouseCallbacks(_gdialog_buttons[0], nullptr, nullptr, nullptr, gameDialogBarterButtonUpMouseUp);
 				buttonSetCallbacks(_gdialog_buttons[0], _gsound_med_butt_press, _gsound_med_butt_release);
