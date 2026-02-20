fbad0778cd09b9301c63dcf72e7d8975cf3200fd
Walter Agazzi <walter.agazzi@protonmail.com>
2026-02-21 00:40:00 +0100

    FALLOUT2: Use texts from proto in multipage loadsave
    From upstream 424f2ffecc0ef96355bb81cc8fbce016a3bd16af
    398c7adb9a05c030854cd82fbf7a704a29e94c4d

--- engines/fallout2/loadsave.cpp
+++ engines/fallout2/loadsave.cpp
@@ -2459,11 +2459,24 @@ static void _ShowSlotList(int windowType) {
 		int activeColor = _colorTable[992];
 		int inactiveColor = _colorTable[8804];
 
-		int backButtonColor = _currentSlotPage > 0 ? activeColor : inactiveColor;
-		fontDrawText(gLoadSaveWindowBuffer + LS_WINDOW_WIDTH * (y + 0) + 95, "BACK", LS_WINDOW_WIDTH, LS_WINDOW_WIDTH, backButtonColor);
+		MessageListItem messageListItemBack;
+		messageListItemBack.num = 654; // Back
+		messageListGetItem(&gProtoMessageList, &messageListItemBack);
+		fontDrawText(
+			gLoadSaveWindowBuffer + LS_WINDOW_WIDTH * (y + 0) + 95,
+			messageListItemBack.text,
+			LS_WINDOW_WIDTH,
+			LS_WINDOW_WIDTH,
+			_currentSlotPage > 0 ? activeColor : inactiveColor);
 
-		int moreButtonColor = _currentSlotPage < saveLoadPages - 1 ? activeColor : inactiveColor;
-		fontDrawText(gLoadSaveWindowBuffer + LS_WINDOW_WIDTH * (y + 0) + 210, "MORE", LS_WINDOW_WIDTH, LS_WINDOW_WIDTH, moreButtonColor);
+		MessageListItem messageListItemMore;
+		messageListItemMore.num = 652; // More
+		messageListGetItem(&gProtoMessageList, &messageListItemMore);
+		fontDrawText(gLoadSaveWindowBuffer + LS_WINDOW_WIDTH * (y + 0) + 210,
+					 messageListItemMore.text,
+					 LS_WINDOW_WIDTH,
+					 LS_WINDOW_WIDTH,
+					 _currentSlotPage < saveLoadPages - 1 ? activeColor : inactiveColor);
 	}
 }
 
